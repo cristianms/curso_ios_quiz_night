6e80f59d2bda006e57d800574958fc1894b20cb9
BCS Automacao <bcsautomacao@MacBook-Air-de-BCS.local>
2018-12-06 22:31:08 -0200

    meu commit inicial

---
+++ QuizNight/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuizNight
+//
+//  Created by BCS Automacao on 06/12/18.
+//  Copyright © 2018 BCS Automacao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
